eb0cb9395ecb9c0ef4c0e8d07846cb55285f7446
Wilco Bonestroo <wilcobonestroo@gmail.com>
2023-12-02 18:26:08 +0100

    Added day 1 solutions

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,51 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.fold(0) { acc, line ->
+            acc + (line.filter { it.isDigit() }.let {
+                "${it.first()}${it.last()}".toInt()
+            })
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val digitMap = mapOf(
+            "one" to "1",
+            "two" to "2",
+            "three" to "3",
+            "four" to "4",
+            "five" to "5",
+            "six" to "6",
+            "seven" to "7",
+            "eight" to "8",
+            "nine" to "9"
+        )
+
+        return input.fold(0) { acc, line ->
+            val digits = line.foldIndexed("") { index, digitAcc, c ->
+                if (c.isDigit()) {
+                    return@foldIndexed digitAcc + c
+                } else {
+                    digitMap.forEach {
+                        if (line.substring(index).startsWith(it.key)) {
+                            return@foldIndexed digitAcc + it.value
+                        }
+                    }
+                }
+
+                digitAcc
+            }
+
+            acc + (digits.let {
+                "${it.first()}${it.last()}".toInt()
+            })
+        }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInputPart1 = readInput("Day01_test1")
+    check(part1(testInputPart1) == 142)
+
+    val testInputPart2 = readInput("Day01_test2")
+    check(part2(testInputPart2) == 281)
 
     val input = readInput("Day01")
     part1(input).println()
