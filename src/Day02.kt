d8a139621f05c28706190a60e92cc40a95ab0e5b
Wilco Bonestroo <wilcobonestroo@gmail.com>
2023-12-03 15:05:12 +0100

    Added day 2 part 2 solution

--- src/Day02.kt
+++ src/Day02.kt
@@ -9,9 +9,9 @@ fun main() {
 
                 subsets.forEach { subset ->
                     val (amount, color) = subset.trim().split(" ")
-                    val isPossible = when(color) {
+                    val isPossible = when (color) {
                         "red" -> amount.toInt() <= 12
-                        "green" ->  amount.toInt() <= 13
+                        "green" -> amount.toInt() <= 13
                         "blue" -> amount.toInt() <= 14
                         else -> true
                     }
@@ -27,14 +27,36 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 1
+        return input.fold(0) { acc, line ->
+            val sets = line.split(":")[1].split(";")
+            val maxColorMap = mutableMapOf<String, Int>()
+
+            sets.forEach { set ->
+                val subsets = set.split(",")
+
+                subsets.forEach { subset ->
+                    val (amount, color) = subset.trim().split(" ")
+                    val currentAmount = maxColorMap.getOrDefault(color, 0)
+
+                    if (currentAmount < amount.toInt()) {
+                        maxColorMap[color] = amount.toInt()
+                    }
+                }
+            }
+
+            val red = maxColorMap.getOrDefault("red", 0)
+            val green = maxColorMap.getOrDefault("green", 0)
+            val blue = maxColorMap.getOrDefault("blue", 0)
+
+            acc + (red * green * blue)
+        }
     }
 
     val testInputPart1 = readInput("Day02_test1")
     check(part1(testInputPart1) == 8)
 
     val testInputPart2 = readInput("Day02_test2")
-    check(part2(testInputPart2) == 1)
+    check(part2(testInputPart2) == 2286)
 
     val input = readInput("Day02")
     part1(input).println()
